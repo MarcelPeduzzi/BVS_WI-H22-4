7b461f471aae7d1bcdb5583b56ccb65c37d2b426
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2023-06-05 21:14:06 +0200

    Lösungen Slides 1+2

---
+++ Loesungen/01-Einführung in Softwarentwicklung/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Dein Name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine("Hi " + name + ", willkommen in der Welt des Programmierens.");
+        Console.WriteLine("");
+        Console.WriteLine("zum Beenden Taste drücken.");
+        Console.ReadKey();
+    }
+}
+
---
+++ Loesungen/02-Variablen und Datentypen/DatenTypen/DatenTypen/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DatenTypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Marcel";
+            string lastName = "Peduzzi";
+            string street = "Dorfstrasse 21";
+            string plz = "9306";
+            string location = "Freidorf";
+            int age = 49;
+            decimal salary = new decimal(5000.50);
+
+            Console.WriteLine("Adresse:");
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(street);
+            Console.WriteLine(plz + " " + location);
+            Console.WriteLine();
+            Console.WriteLine("Alter: " + age);
+            Console.WriteLine("Gehalt: " + salary);
+
+            Console.ReadKey(false);
+        }
+    }
+}
