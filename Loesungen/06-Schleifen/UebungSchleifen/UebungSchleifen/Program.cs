5943122cd0bfe15909f1f8c0a544a84f1c107147
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2023-06-12 20:27:19 +0200

    Uebung Schleifen

---
+++ Loesungen/06-Schleifen/UebungSchleifen/UebungSchleifen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumMinimumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schleife();
+            SchleifeMitMittelwert();
+        }
+
+        static void Schleife()
+        {
+            Console.WriteLine("Schleife");
+            Console.WriteLine("========");
+            for (double d = 35; d >= 20; d -= 2.5)
+            {
+                Console.WriteLine(d);
+            }
+        }
+
+        static void SchleifeMitMittelwert()
+        {
+            Console.WriteLine("Schleife mit Mittelwert");
+            Console.WriteLine("=======================");
+            int count = 0;
+            double summe = 0.0, mw;
+
+            for (double d = 35; d >= 20; d -= 2.5)
+            {
+                Console.WriteLine(d);
+                count = count + 1;
+                summe = summe + d;
+            }
+
+            mw = summe / count;
+            Console.WriteLine( "Summe: " + summe );
+            Console.WriteLine( "Mittelwert: " + mw );
+        }
+    }
+}
